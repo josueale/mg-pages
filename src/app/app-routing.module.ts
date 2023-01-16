b2f418a83b09905feb7fa6f7e9548675fa331980
Josue Ale <jam9814@hotmail.com>
2023-01-16 14:35:56 -0500

    added back Characters resolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
+import { RickAndMortyCharactersResolver } from './shared/services/rick-and-morty-characters.resolver';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {
     path: 'rick-and-morty',
+    resolve: {
+      characters: RickAndMortyCharactersResolver,
+    },
     loadChildren: () =>
-      import('./pages/rick-and-morty/rick-and-morty.module').then((m) => m.RickAndMortyModule),
+      import('./pages/rick-and-morty/rick-and-morty.module').then(
+        (m) => m.RickAndMortyModule
+      ),
   },
 ];
 
